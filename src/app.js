715aaa06cf263523739d4e24735e487ce65b2f0c
Carlos <9671918@dev.azure.com>
2023-12-04 20:16:12 -0300

    Comentarios

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const ProductManager = require('./product-manager.class.js')
 
+//Creamos un objeto de tipo ProductManager
 let productMgr = new ProductManager('./products.json')
 
+//Guardamos en app todas las funciones de Express
 const app = express()
 
+//Agregamos para poder trabajar mejor con la quuery
 app.use(express.urlencoded({ extended: true }))
 
+//Ruta /products/:id a traves de un metodo get, :id deberia ser un ID de un prducto a buscar y devuelve un JSON con
+// un solo producto, en caso de no encontrar concidencia devuelve un 404
 app.get('/products/:id', async (req, res) => {
     let product = await productMgr.getProductById(req.params.id)
     if (product) {
@@ -16,6 +21,8 @@ app.get('/products/:id', async (req, res) => {
     }
 })
 
+//Ruta /products a traves de un metodo get, devuelve un JSON con un array de productos. Podría recibir una
+// query con un valor "limit" que limitaría la cantidad de productos a devolver en el array
 app.get('/products', async (req, res) => {
 
     let { limit } = req.query;
